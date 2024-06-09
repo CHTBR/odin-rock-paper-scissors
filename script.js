1e2313a50582e7d12a4cbe61c28e9a8001270d95
Filip Popelka <117486280+CHTBR@users.noreply.github.com>
2024-06-09 18:07:48 +0200

    Remove comments from script for better readability

--- script.js
+++ script.js
@@ -1,58 +1,45 @@
-// CREATE function getComputerChoice with no arguments
 function getComputerChoice() {
-  // RETURN randomly 'rock', 'paper' or 'scissors'
   moves = ['rock', 'paper', 'scissors'];
   return moves[Math.floor(Math.random()*moves.length)];
 }
-// CREATE function playRound with arguments playerSelection and computerSelection
+
 function playRound(playerSelection, computerSelection) {
-  if (playerSelection === computerSelection) { // IF playerSelection is equal to computerSelection
-    // RETURN text saying the result is a draw
+  if (playerSelection === computerSelection) {
     return `A draw. Both you and the computer played ${playerSelection}.`;
-  } else if ((playerSelection === 'rock' && computerSelection === 'scissors') // IF playerSelection beats computerSelection
+  } else if ((playerSelection === 'rock' && computerSelection === 'scissors')
             || (playerSelection === 'paper' && computerSelection === 'rock')
             || (playerSelection === 'scissors' && computerSelection === 'paper')) {
-    // RETURN text saying player won and what beat what in this case
     return `You won. After all, ${playerSelection} beats ${computerSelection}.`;
-  } else { // IF computerSelection beats playerSelection
-      // RETURN text saying player lost this round and what beats what in this case
+  } else {
       return `You lost. Sadly ${playerSelection} is beaten by ${computerSelection}.`;
   }
 } 
-// CREATE function playGame with no arguments
+
 function playGame() {
-  let playerScore = 0; // INSTANTIATE variable playerScore of type integer with the value of 0
-  let computerScore = 0; // INSTANTIATE variable computerScore of type integer with the value of 0
-  // WHILE playerScore is less than 5 AND computerScore is less than 5
+  let playerScore = 0;
+  let computerScore = 0;
   while (playerScore < 5 && computerScore < 5) {
-    let playerSelection; // INSTANTIATE variable playerSelection
-    // WHILE playerSelection isn't 'rock', 'paper', AND scissors
+    let playerSelection;
     while (playerSelection != 'rock'
           && playerSelection != 'paper'
           && playerSelection != 'scissors') {
-      // SET playerSelection value to GET player input with message asking for their choice of move in lower case
       playerSelection = prompt('What do you play? Rock, paper or scissors?', '').toLowerCase();
     }
-    result = playRound(playerSelection, getComputerChoice()) // INSTANTIATE a variable result of type string with the value of RETURN of playRound with the arguments playerSelection and RETURN of getComputerChoice
-    if (result.includes('won')) { // IF result includes 'won'
-      // INCREMENT playerScore by 1
+    result = playRound(playerSelection, getComputerChoice())
+    if (result.includes('won')) {
       playerScore++;
-    } else if (result.includes('lost')) { // IF result includes 'lost'
-      // INCREMENT computerScore by 1
+    } else if (result.includes('lost')) {
       computerScore++;
     }
-    // OUTPUT result, player score and computer score
     console.group();
     console.log(result);
     console.log(`Your score is ${playerScore}.`);
     console.log(`Computer score is ${computerScore}.`);
     console.groupEnd();
-  if (playerScore === 5) { // IF playerScore is equal to 5
-    // OUTPUT player won message
+  if (playerScore === 5) {
     console.log('You have won.');
-  } else { // IF computerScore is equal to 5
-    // OUTPUT player lost message
+  } else {
     console.log('You have lost.');
   } } }
-// INVOKE playGame
+
 playGame();
